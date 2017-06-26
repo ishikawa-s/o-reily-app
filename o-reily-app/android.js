8a94ed958aa0d410de73716975fc45b8c16f686a
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-06-26 12:38:03 +0900

    add title to head and add testfile

--- o-reily-app/android.js
+++ o-reily-app/android.js
@@ -14,5 +14,8 @@ function hijackLinks(){
 		e.preventDefault();
 		loadPage(e.target.href);
 	});
+	var title = $('h2').html() || 'こんにちは';
+	$('h1').html(title);
+	$('h2').remove();
 	$('#progress').remove();
 }
---
+++ o-reily-app/ch03/android.js
@@ -0,0 +1,61 @@
+﻿/*
+$(document).ready(function(){
+    loadPage(); 
+}); 
+function loadPage(url) {
+    $('body').append('<div id="progress">読み込み中...</div>'); 
+    if (url == undefined) { 
+        $('#container').load('index.html #header ul', hijackLinks);
+    } else { 
+        $('#container').load(url + ' #content', hijackLinks);
+    } 
+} 
+function hijackLinks() {
+    $('#container a').click(function(e){
+        e.preventDefault();
+        loadPage(e.target.href);
+    }); 
+    var title = $('h2').html() || 'こんにちは!'; 
+    $('h1').html(title); 
+    $('h2').remove();
+    $('#progress').remove();
+} 
+*/
+var hist = []; 
+var startUrl = 'index.html'; 
+$(document).ready(function(){
+    loadPage(startUrl);
+});
+function loadPage(url) {
+    $('body').append('<div id="progress">読み込み中...</div>'); 
+    scrollTo(0,0); 
+    if (url == startUrl) {
+        var element = ' #header ul'; 
+    } else {
+        var element = ' #content';
+    }
+    $('#container').load(url + element, function(){
+        var title = $('h2').html() || 'こんにちは!'; 
+        $('h1').html(title); 
+        $('h2').remove(); 
+        $('.leftButton').remove(); 
+        hist.unshift({'url':url, 'title':title}); 
+        if (hist.length > 1) {
+            $('#header').append('<div class="leftButton">'+hist[1].title+'</div>'); 
+            $('#header .leftButton').click(function(e){
+                $(e.target).addClass('clicked');
+                var thisPage = hist.shift(); 
+                var previousPage = hist.shift(); 
+                loadPage(previousPage.url);
+            });
+        } 
+        $('#container a').click(function(e){
+            var url = e.target.href; 
+            if (url.match(/10.113/)) {
+                e.preventDefault(); 
+                loadPage(url);
+            }
+        });
+        $('#progress').remove();
+    });
+}
