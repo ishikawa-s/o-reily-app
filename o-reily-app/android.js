c5d33dceb5a7d0b94342583a32bf9618def55b2f
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-06-26 13:29:26 +0900

    add url check

--- o-reily-app/android.js
+++ o-reily-app/android.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 });
 function loadPage(url){
 	$('body').append('<div id="progress">読みこみ中...</div>');
+	scrollTo(0,0);
 	if(url == undefined){
 		$('#container').load('index.html #header ul', hijackLinks);
 	}else{
@@ -10,9 +11,12 @@ function loadPage(url){
 	}
 }
 function hijackLinks(){
-	$('#conteiner a').click(function(e){
-		e.preventDefault();
-		loadPage(e.target.href);
+	$('#container a').click(function(e){
+		var url = e.target.href;
+		if(url.match(/192.168.15.180/)){
+			e.preventDefault();
+			loadPage(e.target.href);
+		}
 	});
 	var title = $('h2').html() || 'こんにちは';
 	$('h1').html(title);
