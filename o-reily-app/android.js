f7aa49151fdb63027ba0bd4aee55da4a5441f908
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-06-26 11:53:26 +0900

    new html&css&js oreilly h03

--- o-reily-app/android.js
+++ o-reily-app/android.js
@@ -1,12 +1,16 @@
-
-if(window.innerWidth && window.innerWidth <= 480){
-	$(document).ready(function(){
-		$('#header ul').addClass('hide');
-		$('#header').append('<div class="leftButton" onclick="toggleMenu()">メニュー</div>');
-	});
-
-	function toggleMenu(){
-		$('#header ul').toggleClass('hide');
-		$('#header .leftButton').toggleClass('pressed');
+$(document).ready(function{
+	loadPage();
+});
+function loadPage(url){
+	if(url == undifined){
+		$('#container').load('index.html #header ul', hijackLinks);
+	}else{
+		$('#container').load(url + '#content', hijackLinks);
 	}
+}
+function hijackLinks(){
+	$('#conteiner a').click(function(e){
+		e.preventDefault();
+		loadPage(e.target.href);
+	});
 }
