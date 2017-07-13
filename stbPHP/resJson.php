bfd6d557f2f5b41413c1ae2760caff0f455aad2a
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-07-13 14:20:53 +0900

    hun?

--- stbPHP/resJson.php
+++ stbPHP/resJson.php
@@ -1,11 +1,11 @@
 <?php
-var_dump('okok');
 try{
 $con = mysqli_connect('localhost','root','irodoriha37E','ToDoon');
 $db_sel = mysqli_select_db('todo', $con);
 $query = "SELECT name, deadened_id, deadend, detail FROM todo;";
 $res = mysql_query($query);
 $output=array();
+var_dump($res);
 for($i=0; $i<=$output.length; $i++){
 	var_dump($output[i]);
 }
