ef00d4f72c93edbe8577bd648100245a3eba97d9
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-07-13 14:16:30 +0900

    console.logはjsのやつ？いつも忘れるよね

--- stbPHP/resJson.php
+++ stbPHP/resJson.php
@@ -1,14 +1,14 @@
 <?php
-console.log('okok');
+var_dump('okok');
 try{
 $con = mysqli_connect('localhost','root','irodoriha37E','ToDoon');
 $db_sel = mysqli_select_db('todo', $con);
 $query = "SELECT name, deadened_id, deadend, detail FROM todo;";
 $res = mysql_query($query);
 $output=array();
-console.log($output);
+var_dump($output);
 }catch (PDOException $e){
-	console.log($e);
+	var_dump($e);
 }
 while($e=mysqli_fetch_assoc($res)){$output[]=$e;}
 mysqli_free_result($res);
