46711afb25bb4a264d31dcdfb96d0c0435323def
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-07-13 14:19:48 +0900

    aaaaaaaaaaan

--- stbPHP/resJson.php
+++ stbPHP/resJson.php
@@ -6,7 +6,9 @@ $db_sel = mysqli_select_db('todo', $con);
 $query = "SELECT name, deadened_id, deadend, detail FROM todo;";
 $res = mysql_query($query);
 $output=array();
-var_dump($output);
+for($i=0; $i<=$output.length; $i++){
+	var_dump($output[i]);
+}
 }catch (PDOException $e){
 	var_dump($e);
 }
