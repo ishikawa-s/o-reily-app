b26c5d73f87953e092d6a11d69406fe2195aa530
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-07-13 13:45:29 +0900

    exception

--- stbPHP/resJson.php
+++ stbPHP/resJson.php
@@ -1,9 +1,15 @@
 <?php
+
+try{
 $con = mysqli_connect('localhost','root','irodoriha37E','ToDoon');
 $db_sel = mysqli_select_db('todo', $con);
 $query = "SELECT name, deadened_id, deadend, detail FROM todo;";
 $res = mysql_query($query);
 $output=array();
+console.log($output);
+}catch (PDOException $e){
+	console.log($e);
+}
 while($e=mysqli_fetch_assoc($res)){$output[]=$e;}
 mysqli_free_result($res);
 mysqli_close($con);
