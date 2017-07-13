71b42267b26935ff3a728e15f29ba3e4c74a7467
ishikawa-s <ishikawa_s@contents.ne.jp>
2017-07-13 14:27:25 +0900

    dddddddddd

--- stbPHP/resJson.php
+++ stbPHP/resJson.php
@@ -3,7 +3,8 @@ try{
 $con = mysqli_connect('192.168.15.180','root','irodoriha37E','ToDoon');
 $db_sel = mysqli_select_db('todo', $con);
 if(!$con){
-var_dump(mysqli_connect_error().PHP_EOL);
+	echo mysqli_connect_errno().PHP_EOL;
+	echo mysqli_connect_error().PHP_EOL;
 }
 $query = "SELECT name, deadened_id, deadend, detail FROM todo;";
 $res = mysql_query($query);
